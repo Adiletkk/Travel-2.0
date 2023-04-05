de0db41c5f26c1eead208ad4db73ec6c0e60d45f
Adiletkk <adilet_esirkepov02@mail.ru>
2023-04-05 16:28:22 +0500

    create/pages

---
+++ src/components/Pay/Pay.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import css from './Pay.module.scss'
+
+function Pay() {
+  return (
+<div className={css.wrapper}>
+    <div className={css.left}>
+        <h1>askjdhkl</h1>
+        <div className={css.input}></div>
+        <div className={css.left_bottom}>
+
+        </div>
+    </div>
+    <div className={css.right}>
+
+    </div>
+</div>
+  );
+}
+
+export default Pay;
